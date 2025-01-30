6909dd99063e2ed822dd83c6e0a7c564f5f4cfa9
Arseniy <shagako2007@gmail.com>
2025-01-30 11:45:54 +0300

    task 8

--- games/progression.js
+++ games/progression.js
@@ -1,4 +1,4 @@
-import { getRandomInt } from '..src/index.js'
+import { getRandomInt } from '../src/index.js'
 import readlineSync from 'readline-sync'
 export const progressionGame = () => {
     console.log('Welcome to the Brain Games!');
@@ -8,5 +8,29 @@ export const progressionGame = () => {
         const firstNumber = getRandomInt(1, 5)
         const progressionLength = getRandomInt(5, 12)
         const progressionStep = getRandomInt(2, 5)
+        const hiddenNumberPosition = getRandomInt(1, progressionLength)
+        let count = firstNumber
+        let progressionList = []
+        let correctAnswer = 0 
+        for (let i = 1; i <= progressionLength; i += 1) {
+            if (i === hiddenNumberPosition) {
+                correctAnswer = count
+                progressionList.push('..')
+                count += progressionStep
+            }
+            else {
+                progressionList.push(count)
+                count += progressionStep
+            }
+        }
+        console.log(`Question ${progressionList.join(' ')}`)
+        const userAnswer = readlineSync.question('Your answer:');
+        if (+userAnswer === correctAnswer) {
+            console.log('Correct!')
+        }
+        else {
+            return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}' \nLet's try again, ${userName}!`);
+        }
     }
+    return console.log(`Congratulations,${userName}`);
 }
